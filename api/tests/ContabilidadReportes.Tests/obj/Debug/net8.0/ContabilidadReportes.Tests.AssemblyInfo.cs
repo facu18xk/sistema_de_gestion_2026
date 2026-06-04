b748f8989bb2985d67980355f8ebfaa2b6b4930a
Facu <5675299@mec.edu.py>
2026-06-04 11:26:46 -0300

    Fix: rrhh stuff

--- api/tests/ContabilidadReportes.Tests/obj/Debug/net8.0/ContabilidadReportes.Tests.AssemblyInfo.cs
+++ api/tests/ContabilidadReportes.Tests/obj/Debug/net8.0/ContabilidadReportes.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063aadfcecac0e533d87a7fe5f229225b5702131")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddd182631173e8e5e4bdea62ad3c8587952ff930")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
