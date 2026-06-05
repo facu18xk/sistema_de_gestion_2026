843057e8ee2e426857fddc4247a0d1fbb9310902
Facu <5675299@mec.edu.py>
2026-06-05 10:09:54 -0300

    Fix: bugs in contabilidad

--- api/tests/ContabilidadReportes.Tests/obj/Debug/net8.0/ContabilidadReportes.Tests.AssemblyInfo.cs
+++ api/tests/ContabilidadReportes.Tests/obj/Debug/net8.0/ContabilidadReportes.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbbefa478e46c3beca7707f0a9c0c56a24e6f82b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e88685f29ee350ffeebdb3987e83b62a7b9b5c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContabilidadReportes.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
